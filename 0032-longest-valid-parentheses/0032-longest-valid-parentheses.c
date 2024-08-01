61e80b60d25f10aec76ba944ca57f5524634d657
SHERIN MARIA R <sherinmaria456@gmail.com>
2024-08-01 12:21:18 +0530

    Time: 0 ms (100%), Space: 8.9 MB (7.61%) - LeetHub

---
+++ 0032-longest-valid-parentheses/0032-longest-valid-parentheses.c
@@ -0,0 +1,25 @@
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+
+int longestValidParentheses(char* s) {
+    int max_length = 0;
+    int* stack = (int*)malloc(sizeof(int) * (strlen(s) + 1));
+    int top = -1;
+
+    stack[++top] = -1;
+
+    for (int i = 0; s[i] != '\0'; i++) {
+        if (s[i] == '(') {
+            stack[++top] = i;
+        } else {
+            top--;
+            if (top == -1) {
+                stack[++top] = i; 
+            } else {
+                max_length = MAX(max_length, i - stack[top]);
+            }
+        }
+    }
+
+    free(stack);
+    return max_length;
+}
