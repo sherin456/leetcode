ba05d1648187961e7e77b871911afd78bdec67be
SHERIN MARIA R <sherinmaria456@gmail.com>
2024-08-01 12:23:11 +0530

    Time: 54 ms (56.05%), Space: 15.1 MB (6.45%) - LeetHub

---
+++ 0041-first-missing-positive/0041-first-missing-positive.c
@@ -0,0 +1,11 @@
+int firstMissingPositive(int* nums, int size){
+    int a = INT_MAX;
+    int *n = (int*)calloc(size, 4);
+    for(int i = 0; i < size ; i++)
+        if (nums[i] <= size && nums[i] > 0)
+            n[nums[i]-1] = nums[i];
+    for(int i = 0 ; i < size; i++)
+        if (n[i] == 0)
+            return i+1;
+    return size + 1;
+}
